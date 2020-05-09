65e4550e03132ce047d70920a7cb83e62e49f42e
Philou <philippe.fuentes@vetup.com>
2020-05-09 10:52:05 +0200

    doc

--- build_eussam.sh
+++ build_eussam.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# PF: 30/06/2019  copie du rep qmk_firmware pour compiler mes layout facilement
-# épuration du repo un max pour accélérer la comilation avec docker et vider le repo d'origine
-# déplacement du fichier hex eussam généré dans répertoire ./hex
-# TODO: épurer encore plus le rép qmk_firmware pour virer tout ce dont je ne me sers pas
+# - si les sous module git ne sont pas à jour, lancer 'make git-submodule'
+# - pour synchroniser la branche master avec le master de zsa: 
+#     git fetch upstream
+#     git merge upstream/master
 
 # cd qmk_firmware
 ./util/docker_build_osx.sh ergodox_ez:bepo_eussam
